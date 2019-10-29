af470e3e7aae57e1882611c979f65e7a1a0a8122
Bryan Casler <bryan.casler@gmail.com>
2019-10-29 15:20:16 -0400

    Updates based on theme

--- src/ts/app/utils/custom-methods.ts
+++ src/ts/app/utils/custom-methods.ts
@@ -168,14 +168,15 @@ export const debugBar = () => {
     location.hostname === "localhost" ||
     location.hostname === "127.0.0.1"
   ) {
-    body.classList.add("debug-enabled");
-    body.addEventListener("mouseenter", debugStart);
-    body.addEventListener("mouseleave", debugStop);
+    body.classList.add("debug-disabled");
+    body.classList.add("debug-off");
+    // body.addEventListener("mouseenter", debugStart);
+    // body.addEventListener("mouseleave", debugStop);
     if (enGrid) {
       enGrid.insertAdjacentHTML(
         "afterend",
         '<span id="debug-bar">' +
-          '<button id="debug-toggle" type="button">Turn Debug Off</button>' +
+          '<button id="debug-toggle" type="button">Turn Debug On</button>' +
           '<button id="layout-toggle" type="button">Layout Toggle</button>' +
           "</span>"
       );
@@ -262,7 +263,7 @@ export const debugBar = () => {
 export const inputPlaceholder = () => {
   const enGridFloatLabels = document.querySelector("#engrid:not(.float-labels)") as HTMLElement;
   if (enGridFloatLabels) {
-    // const enFieldDonationAmt = document.querySelector(".en__field--donationAmt.en__field--withOther .en__field__input--other") as HTMLInputElement;
+    const enFieldDonationAmt = document.querySelector(".en__field--donationAmt.en__field--withOther .en__field__input--other") as HTMLInputElement;
     const enFieldFirstName = document.querySelector("#en__field_supporter_firstName") as HTMLInputElement;
     const enFieldLastName = document.querySelector("#en__field_supporter_lastName") as HTMLInputElement;
     const enFieldEmailAddress = document.querySelector("#en__field_supporter_emailAddress") as HTMLInputElement;
@@ -291,9 +292,10 @@ export const inputPlaceholder = () => {
     const enFieldBankAccountNumber = document.querySelector("#en__field_supporter_bankAccountNumber") as HTMLInputElement;
     const enFieldBankRoutingNumber = document.querySelector("#en__field_supporter_bankRoutingNumber") as HTMLInputElement;
 
-    // if (enFieldDonationAmt) {
-    //   enFieldDonationAmt.placeholder = "Other";
-    // }
+    if (enFieldDonationAmt) {
+      enFieldDonationAmt.placeholder = "Other";
+      enFieldDonationAmt.setAttribute("type", "number");
+    }
     if (enFieldFirstName) {
       enFieldFirstName.placeholder = "First name";
     }
