d5ca0e608a9b95d22d2a017ee244c046dcdda45e
Bryan Casler <bryan.casler@gmail.com>
2019-11-01 15:44:18 -0400

    Added support for One-time vs Monthly hide and show as well as legacy support for GiveBySelect

--- src/ts/app/index.ts
+++ src/ts/app/index.ts
@@ -14,6 +14,8 @@ export const run = () => {
   app.enInput.init();
 
   new ShowHideRadioCheckboxes("transaction.giveBySelect", "giveBySelect-");
+  new ShowHideRadioCheckboxes("supporter.questions.180165", "giveBySelect-");  
   new ShowHideRadioCheckboxes("transaction.inmem", "inmem-");
+  new ShowHideRadioCheckboxes("transaction.recurrpay", "recurrpay-");
   app.debugBar();
 };
