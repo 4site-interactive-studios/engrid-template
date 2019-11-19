6b71199e652aea48c460baf366c1554725cf8c79
Fernando Santos <fern@ndo.io>
2019-11-19 17:03:57 -0300

    Removing old code

--- src/ts/app/events/donation-form.ts
+++ src/ts/app/events/donation-form.ts
@@ -1,20 +1,11 @@
 import { SignalDispatcher } from "strongly-typed-events";
-import { amount } from "../index";
-import { frequency } from "../index";
 
 export default class DonationForm {
   private _onSubmit = new SignalDispatcher();
   private _onError = new SignalDispatcher();
-  private _submitBtn = document.querySelector(
-    ".en__submit button"
-  ) as HTMLElement;
 
   public dispatchSubmit() {
     this._onSubmit.dispatch();
-
-    if (this._submitBtn) {
-      return true;
-    }
   }
   public dispatchError() {
     this._onError.dispatch();
