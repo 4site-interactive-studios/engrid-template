b0103725ec2d6e47ed24899302e52295980542dd
Bryan Casler <bryan.casler@gmail.com>
2020-01-27 18:16:43 -0500

    Improved Background Image logic for legacy pages

--- src/themes/ran/index.ts
+++ src/themes/ran/index.ts
@@ -1,7 +1,11 @@
 import { run } from "../../ts/app";
 import "./sass/main.scss";
 
-const options = {};
+const options = {
+  backgroundImage: [
+  "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10042/IMG-3019_Greenpeace_Victor_Moriyama-BACKGROUND.jpg?v=1572910092000"
+  ]
+};
 
 if (document.readyState !== "loading") {
   run(options);
