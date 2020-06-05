044dc4c5958a9ee4d26279f861bf6bda633735f9
Bryan Casler <bryan.casler@gmail.com>
2020-06-04 20:31:17 -0400

    CSS Vairable Sub-themes are here!

--- src/themes/earthisland/index.ts
+++ src/themes/earthisland/index.ts
@@ -2,9 +2,9 @@ import { run } from "../../ts/app";
 import "./sass/main.scss";
 
 const options = {
-  backgroundImage: [
-    "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10069/earthisland-background.jpg"
-  ],
+  // backgroundImage: [
+  //   "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10069/earthisland-background.jpg"
+  // ],
   submitLabel: "Give"
 };
 
