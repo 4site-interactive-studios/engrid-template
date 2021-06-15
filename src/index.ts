25001206258f539baf9865c2cd86820c3c8ce268
bryancasler <bryan.casler@gmail.com>
2021-06-15 14:57:28 -0400

    Overhaul of testing set up and default to using local ENGrid Scripts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
-// import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
+// import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
+import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 
 const options: Options = {
