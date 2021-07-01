0f1f5d57a1711a7089bba1f1432b8bf08f72b2c0
bryancasler <bryan.casler@gmail.com>
2021-07-01 12:51:35 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const options: Options = {
   MediaAttribution: true,
   SkipToMainContentLink: true,
   SrcDefer: true,
+  ProgressBar: true,
   Debug: App.getUrlParameter('debug') == 'true' ? true : false,
   onLoad: () => console.log("Starter Theme Loaded"),
   onResize: () => console.log("Starter Theme Window Resized")
